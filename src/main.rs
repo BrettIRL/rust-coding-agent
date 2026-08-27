29f83d9fdddf43736cc4f3aed3c68829961a2287
Brett Cocking <brett@cocking.dev>
2026-08-27 11:29:38 +0100

    remove debug message

--- src/main.rs
+++ src/main.rs
@@ -94,9 +94,6 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
             }))
             .await?;
 
-        // You can use print statements as follows for debugging, they'll be visible when running tests.
-        eprintln!("Logs from your program will appear here!");
-
         let message = &response["choices"][0]["message"];
         messages.push(message.clone());
 
